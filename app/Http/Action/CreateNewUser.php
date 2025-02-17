af246817ea3c9a4dcc8018664ff31796f548b0ce
rishni <ashameemeduma@gmail.com>
2025-02-17 09:26:15 +0000

    check pint

---
+++ app/Http/Action/CreateNewUser.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Action;
+
+use App\Models\User;
+
+class createNewUser
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function createNewUser()  {
+        return [
+            'newUser' => 'newUser',
+        ];
+    }
+}
